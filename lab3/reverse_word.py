49d6ea30adab184cf2c9db1e927c41355af396a8
Markaaaaaa <115102897+Markaaaaaa@users.noreply.github.com>
2024-10-22 21:36:48 -0400

    Update reverse_word.py

--- lab3/reverse_word.py
+++ lab3/reverse_word.py
@@ -2,7 +2,7 @@ def reverse_string(s):
     return s[::-1]
 
 
-from practical_Lab.readfile import read
-file_path = 'example'
-content = read(file_path)
-print(content)
+from reverse_string import reverse_string
+user_input = input("Enter a string to reverse: ")
+reversed_string = reverse_string(user_input)
+print(f"Reversed string: {reversed_string}")

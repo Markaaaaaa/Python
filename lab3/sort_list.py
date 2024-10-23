794e64923f5e17f61d0f3deffb48d616d17b0ab5
Markaaaaaa <115102897+Markaaaaaa@users.noreply.github.com>
2024-10-22 21:40:57 -0400

    Create sort_list.py

---
+++ lab3/sort_list.py
@@ -0,0 +1,9 @@
+def sort_numbers(num_list):
+    return sorted(num_list)
+
+
+
+from sort_list import sort_numbers
+numbers = [8, 3, 5, 2, 9, 1]
+sorted_numbers = sort_numbers(numbers)
+print(f"Sorted list: {sorted_numbers}")

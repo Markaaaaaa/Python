836a9bd68c856a18c69dca835ebad4133ce2fd38
Markaaaaaa <115102897+Markaaaaaa@users.noreply.github.com>
2024-10-27 17:07:22 -0400

    Create 4.py

---
+++ Lab4/4.py
@@ -0,0 +1,12 @@
+class Cat:
+    def make_sound(self):
+        print("Miu")
+
+class Dog:
+    def make_sound(self):
+        print("Bark")
+
+dog=Dog()
+cat=Cat()
+dog.make_sound()
+cat.make_sound()

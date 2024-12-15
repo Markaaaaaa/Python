8017d9ea062d4c2383411c18e6eb0efbe15d458f
Markaaaaaa <115102897+Markaaaaaa@users.noreply.github.com>
2024-12-15 13:28:52 -0500

    Update Find the Missing Number .py

--- Lab9/Find the Missing Number .py
+++ Lab9/Find the Missing Number .py
@@ -1,11 +1,11 @@
 def missing_number(numbers, n):
-    expected_sum = n * (n + 1) // 2
+    expected_sum = n * (n + 1) // 2 #find the sum from 1 to the length of the string
     actual_sum = sum(numbers)
     return expected_sum - actual_sum
 
 # Input and Output
-numbers = list(map(int, input("Enter numbers separated by spaces: ").split()))
-n = len(numbers) + 1
+numbers = list(map(int, input("Enter numbers separated by spaces: ").split())) #make it a list
+n = len(numbers) + 1 #there is a missing number 
 print(len(numbers))
 number = missing_number(numbers, n)
 print(f"The missing number is: {number}")

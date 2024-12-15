629777bce2e7f4401e2100c9018a90a5fbef29c6
Markaaaaaa <115102897+Markaaaaaa@users.noreply.github.com>
2024-12-15 13:27:10 -0500

    Update Prime Number Checker .py

--- Lab9/Prime Number Checker .py
+++ Lab9/Prime Number Checker .py
@@ -1,7 +1,7 @@
 def prime(number):
     if number <= 1:
         return False
-    for i in range(2, int(number ** 0.5) + 1):
+    for i in range(2, int(number ** 0.5) + 1): #a number has a divisor larger than its square root, the corresponding smaller divisor would have already been found.
         if number % i == 0:
             return False
     return True
